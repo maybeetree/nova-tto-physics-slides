d86c043caa3a14617a773ef113a6f84fc55a7539
maybetree <maybetree48@proton.me>
2024-09-11 11:22:30 +0200

    fix lua filter

--- filters/missing-images.lua
+++ filters/missing-images.lua
@@ -1,4 +1,17 @@
-function Image(el)
-	return "**MISSING IMAGE**"
+local function file_exists(name)
+   local f = io.open(name, "r")
+   if f ~= nil then
+      io.close(f)
+      return true
+   else
+      return false
+   end
 end
 
+function Image(el)
+  if not file_exists(el.src) then
+    return pandoc.Str("MISSING IMAGE: " .. el.src)
+  else
+    return el
+  end
+end
